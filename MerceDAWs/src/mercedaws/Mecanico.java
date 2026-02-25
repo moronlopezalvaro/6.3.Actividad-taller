9f940828e2c03c29c8a38bf3a96ac8b6152add1c
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-02-25 20:20:22 +0100

    class Mecanico

---
+++ MerceDAWs/src/mercedaws/Mecanico.java
@@ -0,0 +1,15 @@
+package mercedaws;
+
+public class Mecanico implements Especialista {
+
+    private String nombre;
+
+    public Mecanico(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void reparar(Reparacion r) {
+        // Lógica futura de reparación
+    }
+}
